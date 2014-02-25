31f86e858307af645f0aac0f7a4a56e26bea60c3
DPL-1 <DPL-1@DPL-1s-iMac.local>
2014-02-25 10:27:44 -0800

    adds condition for chef controller create action

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,11 +1,22 @@
 class ChefsController < ApplicationController
-  
+
+
   def new
     @chef = Chef.new
   end
 
+  def show
+  end
+
   def create
     @chef = Chef.new(chef_params)
+    if @chef.save
+      flash[:success] = 'Chef #{@chef.humanize} was saved'
+      redirect_to chef_path(@chef.id)
+    else
+      flash[:error] = 'Chef #{@chef.humanize} was not saved'
+      render :new
+    end
     raise @chef.inspect
   end
 
