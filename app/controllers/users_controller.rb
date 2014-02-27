da5e82aa80f0b0cd59f66a350ccecb2f52d844d5
DPL-1 <DPL-1@DPL-1s-iMac.local>
2014-02-27 10:13:55 -0800

    builds out users controller and creates users/new.html.erb file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.user_id
+      redirect_to root_path, notice: 'You have signed up'
+    else
+      
+    end
+  end
+
+  private
+  def user_params
+    @user = User.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
