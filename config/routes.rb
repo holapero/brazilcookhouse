537f22b3a533c7051e77bfa21898c7ab0386db1f
DPL-1 <DPL-1@DPL-1s-iMac.local>
2014-02-27 10:28:19 -0800

    adds users routes, and builds out user/new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 BrazillianCookery::Application.routes.draw do
   resources :chefs
   resources :recipes
+  resources :users, only:[:new, :create]
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
 
